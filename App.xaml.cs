140eb156642b5e6c17c0d5cb61ed186079f64b92
zixing <1311817771@qq.com>
2025-03-31 21:32:40 +0800

    初始化项目
    初始化项目

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MT6252_Simulator_Sharp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
